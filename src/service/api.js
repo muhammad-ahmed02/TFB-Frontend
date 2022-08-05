aa6a690e0e1c893e80e01d6fe62e30e106c5a17f
umer2001 <umer2001.uf@gmail.com>
2022-08-05 20:55:51 +0500

    add: api calls

--- src/service/api.js
+++ src/service/api.js
@@ -24,4 +24,38 @@ export const register = (registerdata) =>
     return res.data;
   });
 
+// Settings
+export const getSettings = (id) => api.get(`/settings/${id}/`).then((res) => res.data);
+export const updateSettings = (id, settings) => api.put(`/settings/${id}/`, settings).then((res) => res.data);
+
+// Products
+export const getProducts = () => api.get('/products/').then((res) => res.data);
+export const getProduct = (id) => api.get(`/products/${id}/`).then((res) => res.data);
+export const createProduct = (product) => api.post('/products/', product).then((res) => res.data);
+export const updateProduct = (id, product) => api.put(`/products/${id}/`, product).then((res) => res.data);
+export const deleteProduct = (id) => api.delete(`/products/${id}/`).then((res) => res.data);
+
+// Sellers
+export const getSellers = () => api.get('/seller-profile/').then((res) => res.data);
+export const getSeller = (id) => api.get(`/seller-profile/${id}/`).then((res) => res.data);
+export const createSeller = (seller) => api.post('/seller-profile/', seller).then((res) => res.data);
+export const updateSeller = (id, seller) => api.put(`/seller-profile/${id}/`, seller).then((res) => res.data);
+export const deleteSeller = (id) => api.delete(`/seller-profile/${id}/`).then((res) => res.data);
+
+// Cash Orders
+export const getCashOrders = () => api.get('/cashorder/').then((res) => res.data);
+export const getCashOrder = (id) => api.get(`/cashorder/${id}/`).then((res) => res.data);
+export const createCashOrder = (cashorder) => api.post('/cashorder/', cashorder).then((res) => res.data);
+export const updateCashOrder = (id, cashorder) => api.put(`/cashorder/${id}/`, cashorder).then((res) => res.data);
+export const deleteCashOrder = (id) => api.delete(`/cashorder/${id}/`).then((res) => res.data);
+
+// Return cash orders
+export const getReturnCashOrders = () => api.get('/return-cashorder/').then((res) => res.data);
+export const getReturnCashOrder = (id) => api.get(`/return-cashorder/${id}/`).then((res) => res.data);
+export const createReturnCashOrder = (returncashorder) =>
+  api.post('/return-cashorder/', returncashorder).then((res) => res.data);
+export const updateReturnCashOrder = (id, returncashorder) =>
+  api.put(`/return-cashorder/${id}/`, returncashorder).then((res) => res.data);
+export const deleteReturnCashOrder = (id) => api.delete(`/return-cashorder/${id}/`).then((res) => res.data);
+
 export const getUser = () => api.get(`/auth/user/`);
