943c46f0fb598193fee50964a6cfafc555338010
umer2001 <umer2001.uf@gmail.com>
2022-08-12 16:58:30 +0500

    delete file

--- src/pages/cashorders/components/CashOrderSchema.js
+++
@@ -1,9 +0,0 @@
-import * as Yup from 'yup';
-
-export const CashOrderSchema = Yup.object().shape({
-  customer_name: Yup.string().nullable(),
-  product: Yup.number().required('Product is required'),
-  sale_by: Yup.number().required('Sale by is required'),
-  sale_price: Yup.number().required('Sale price is required'),
-  warranty: Yup.number().nullable(),
-});
